291a0c51f7b5da8169946184b76a18012a367572
alisharuf <56736496+alisharuf@users.noreply.github.com>
2023-12-06 11:41:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/* collecting id's of input search bar and list of iteam */
 const inputText = document.getElementById('input-txt');
 const taskList = document.getElementById('task');
 
+/* function to collect peding task from user from input and adding it to the list*/
 function addTask() {
     if (inputText.value === '') {
         alert('atleast give some input to display on TODO list');
@@ -14,8 +16,10 @@ function addTask() {
         li.appendChild(span);
     }
     inputText.value = "";
-    saveData();
+    saveData();/* function call to save todo data in internal storage of browser*/
 }
+
+/* Function to add, make it checked(onclicking) and to delete that task from todo list*/
 taskList.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("selected");
@@ -25,6 +29,9 @@ taskList.addEventListener("click", function (e) {
         saveData();
     }
 }, false);
+
+/* function definition to save todo data in internal storage of browser so that if we 
+   refresh the browser it'll be there in the page , it's not going to be vanish*/
 function saveData() {
     localStorage.setItem("data", taskList.innerHTML);
 }
